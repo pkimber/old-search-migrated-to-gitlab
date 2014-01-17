0d2c54816125140de1d7385d37b72d517a8907bb
Patrick Kimber <code@pkimber.net>
2014-01-17 16:35:10 +0100

    version 0.0.02

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from distutils.core import setup
+
+
+def read_file_into_string(filename):
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+def get_readme():
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file_into_string(name)
+    return ''
+
+
+setup(
+    name='pkimber-search',
+    packages=['search', 'search.tests'],
+    package_data={
+        'search': [
+            'templates/*.*',
+            'templates/search/*.*',
+        ],
+    },
+    version='0.0.02',
+    description='Search',
+    author='Patrick Kimber',
+    author_email='code@pkimber.net',
+    url='git@github.com:pkimber/search.git',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business :: Scheduling',
+    ],
+    long_description=get_readme(),
+)
