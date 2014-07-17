8f312159f97adbbb05b9e698a0172103f318a3c8
Patrick Kimber <code@pkimber.net>
2014-07-17 21:08:06 +0100

    version 0.1.02

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/search/*.*',
         ],
     },
-    version='0.1.01',
+    version='0.1.02',
     description='Search',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
