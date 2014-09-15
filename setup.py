943654dc5671c61515e484c507a2d8a688f1106c
Malcolm Dinsmore <m.dinsmore@talk21.com>
2014-09-15 12:22:25 +0100

    version 0.1.10

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/search/*.*',
         ],
     },
-    version='0.1.09',
+    version='0.1.10',
     description='Search',
     author='Malcolm Dinsmore',
     author_email='m.dinsmore@talk21.com',
